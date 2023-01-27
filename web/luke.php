d66697d71b22816d957c410c792f7a84fcae168c
Luke Wery <luke.wery2019@gmail.com>
2023-01-27 12:04:10 -0600

    Contributing luke.php

---
+++ web/luke.php
@@ -0,0 +1,21 @@
+<?php
+
+require('../vendor/autoload.php');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '\..');
+$dotenv->safeLoad();
+$times = $_ENV['TIMES'];
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Test</title>
+</head>
+<body>
+<h1>Luke Wery</h1>
+<?php echo "<p>" . str_repeat("Hello", $times) . "</p>" ?>
+</body>
+</html>
