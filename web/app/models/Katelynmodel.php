e9f64a7afe87936abc64ad6fab76e6af02077d6a
Katelyn Owen <katelynrowen@gmail.com>
2023-02-24 13:38:30 -0600

    Katelyn's MVC Demo

---
+++ web/app/models/Katelynmodel.php
@@ -0,0 +1,15 @@
+<?php
+
+class Katelynmodel {
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+
+    public function getAllUsers() {
+        $this->db->query( "SELECT * FROM user_profile");
+        return $this->db->resultSet();
+
+    }
+}
