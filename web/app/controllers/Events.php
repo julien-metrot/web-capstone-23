cc711fcea83df60b6b59e7e068779d2ce8d8b2e9
Luke Wery <luke.wery2019@gmail.com>
2023-03-09 12:36:06 -0600

    Luke's events

---
+++ web/app/controllers/Events.php
@@ -0,0 +1,15 @@
+<?php
+ class Events extends Controller {
+     public function __construct() {
+         $this->eventsModel = $this->model("Eventsmodel");
+     }
+     public function upcoming () {
+         $events = $this->eventsModel->getAllEvents();
+         $data = [
+             "title" => "Upcoming events",
+             "events" => $events
+         ];
+         $this->view("events/upcoming", $data);
+     }
+
+ }
