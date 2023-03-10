13f10449dc71b2d6ccb22f048c57bb290cc73cae
April Rieff <april.rieff@gmail.com>
2023-03-10 11:22:22 -0600

    Added user_id to page

--- web/app/models/Adoptmodel.php
+++ web/app/models/Adoptmodel.php
@@ -10,7 +10,7 @@ class Adoptmodel
 
     public function getAllApplications() {
         $this->db->query("
-            SELECT u.firstname, u.lastname, a.animal_id, a.name, a.animal_id, app.application_status, app.employer_name, app.has_children, app.home_status, app.landlord_name, app.landlord_phone, app.current_pets
+            SELECT u.firstname, u.lastname, a.animal_id, a.name, u.user_id, app.application_status, app.employer_name, app.has_children, app.home_status, app.landlord_name, app.landlord_phone, app.current_pets
             FROM user_profile AS u 
                 INNER JOIN application AS app ON u.user_id = app.user_id
                 INNER JOIN animal AS a ON app.animal_id = a.animal_id
--- web/app/views/adopt/applications.php
+++ web/app/views/adopt/applications.php
@@ -5,15 +5,15 @@
 ?>
 <?php require_once(APPROOT . "/views/inc/header.php") ?>
         <?php foreach($data["applications"] as $applications): ?>
-        <h2><?php echo $applications->firstname . " " . $applications->lastname; ?></h2>
-        <p>Interested in: <?php echo $applications->name . " (" . $applications->animal_id . ")"; ?>
-            Application status: <?php echo $applications->application_status; ?>
-            Employer Name: <?php echo $applications->employer_name; ?>
-            Does this applicant have children in their home?: <?php echo $applications->has_children; ?>
-            Does this applicant currently rent or own their home?: <?php echo $applications->home_status; ?>
-            Landlord Name: <?php echo $applications->landlord_name; ?>
-            Landlord Phone Number: <?php echo $applications->landlord_phone; ?>
-            Does this applicant currently have pets?: <?php echo $applications->current_pets; ?>
+        <h2><?php echo $applications->firstname . " " . $applications->lastname . " ($applications->user_id)"; ?></h2>
+        <p>Interested in: <?php echo $applications->name . " (" . $applications->animal_id . ")"; ?><br>
+            Application status: <?php echo $applications->application_status; ?><br>
+            Employer Name: <?php echo $applications->employer_name; ?><br>
+            Does this applicant have children in their home?: <?php echo $applications->has_children; ?><br>
+            Does this applicant currently rent or own their home?: <?php echo $applications->home_status; ?><br>
+            Landlord Name: <?php echo $applications->landlord_name; ?><br>
+            Landlord Phone Number: <?php echo $applications->landlord_phone; ?><br>
+            Does this applicant currently have pets?: <?php echo $applications->current_pets; ?><br>
         </p>
         <?php endforeach; ?>
 
