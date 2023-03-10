9df8725e534e7cf7edc677504f498067d66dcc29
Marc Hauschildt <marc.hauschildt@gmail.com>
2023-03-10 13:43:40 -0600

    setup blog page

--- web/app/views/inc/footer.php
+++ web/app/views/inc/footer.php
@@ -1,2 +1,9 @@
+<script src="<?php echo URLROOT; ?>/vendor2/jquery/jquery.min.js"></script>
+<script src="<?php echo URLROOT; ?>/vendor2/bootstrap/js/bootstrap.min.js"></script>
+<!-- Custom Js -->
+<script src="<?php echo URLROOT; ?>/js/custom.js"></script>
+<script src="<?php echo URLROOT; ?>/js/plugins.js"></script>
+<!-- Prefix free -->
+<script src="<?php echo URLROOT; ?>/js/prefixfree.min.js"></script>
 </body>
 </html>
