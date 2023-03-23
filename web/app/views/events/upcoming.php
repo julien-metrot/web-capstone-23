6f32a593c73468ea4ce1a566a855dc1c9cd21957
Luke Wery <luke.wery2019@gmail.com>
2023-03-23 18:46:58 -0500

    image

--- web/app/views/events/upcoming.php
+++ web/app/views/events/upcoming.php
@@ -5,37 +5,37 @@
 ?>
 <?php require_once(APPROOT . "/views/inc/header.php") ?>
 
-    <!-- Jumbotron -->
-    <div class="jumbotron jumbotron-fluid" data-center="background-size: 100%;"
-         data-top-bottom="background-size: 110%;">
-        <div class="container" >
-            <div class="jumbo-heading" data-aos="fade-up">
-                <!-- jumbo-heading -->
-                <h1>Events</h1>
-                <!-- Breadcrumbs -->
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Events</li>
-                    </ol>
-                </nav>
-                <!-- /breadcrumb -->
-            </div>
-            <!-- /jumbo-heading -->
+<!-- Jumbotron -->
+<div class="jumbotron jumbotron-fluid" data-center="background-size: 100%;"
+     data-top-bottom="background-size: 110%;">
+    <div class="container" >
+        <div class="jumbo-heading" data-aos="fade-up">
+            <!-- jumbo-heading -->
+            <h1>Events</h1>
+            <!-- Breadcrumbs -->
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Events</li>
+                </ol>
+            </nav>
+            <!-- /breadcrumb -->
         </div>
-        <!-- /container -->
+        <!-- /jumbo-heading -->
     </div>
-    <!-- /jumbotron -->
-    <!-- ==== Page Content ==== -->
-    <div class="page container">
-        <div class="row">
-            <!-- page  starts -->
-            <div class="col-lg-12">
-                <h2>Adoption Events</h2>
-                <span class="h7 mt-0">Adoption is an act of love, join one of our events and find your new best friend </span>
-                <hr class="small-divider left">
-                <div class="row mt-5">
-<?php foreach($data["events"] as $event): ?>
+    <!-- /container -->
+</div>
+<!-- /jumbotron -->
+<!-- ==== Page Content ==== -->
+<div class="page container">
+    <div class="row">
+        <!-- page  starts -->
+        <div class="col-lg-12">
+            <h2>Adoption Events</h2>
+            <span class="h7 mt-0">Adoption is an act of love, join one of our events and find your new best friend </span>
+            <hr class="small-divider left">
+            <div class="row mt-5">
+                <?php foreach($data["events"] as $event): ?>
                     <!-- event 1 -->
                     <div class="col-lg-4 widget2">
                         <div class="card bg-light" >
@@ -61,29 +61,20 @@
                         </div>
                         <!-- /card -->
                     </div>
-<?php endforeach; ?>
+                <?php endforeach; ?>
 
-                </div>
-                <!-- row -->
-                <div class="col-md-12 mt-5">
-                    <!-- pagination -->
-                    <!-- /nav -->
-                </div>
-                <!-- /col-md -->
             </div>
-            <!-- /col-lg-12 -->
+            <!-- row -->
+            <div class="col-md-12 mt-5">
+                <!-- pagination -->
+                <!-- /nav -->
+            </div>
+            <!-- /col-md -->
         </div>
-        <!-- /row -->
+        <!-- /col-lg-12 -->
     </div>
-    <!-- /page -->
+    <!-- /row -->
+</div>
+<!-- /page -->
 
 <?php require_once(APPROOT . "/views/inc/footer.php") ?>
-
-
-<?php //foreach($data["events"] as $event): ?>
-<!--    <h3>--><?php //echo $event->title ?><!--</h3>-->
-<!--      <p>Date: --><?php //echo $event->date ?><!--<br>-->
-<!--        Name: --><?php //echo $event->name ?><!--<br>-->
-<!--        Image: <br>-->
-<!--        Address: --><?php //echo $event->address ?><!-- <br>-->
-<!--    </p>-->
