be3c9be80c56a807792588d684244c8b8c04f87b
Julien Metrot <jul.metrot@gmail.com>
2023-03-24 13:09:28 -0500

    Added the registration form

---
+++ web/app/controllers/User.php
@@ -0,0 +1,89 @@
+<?php
+class User extends Controller {
+    public function __construct() {
+        $this->userModel = $this->model("Users");
+    }
+
+    public function register() {
+        $data = [
+            "title" => "Register an Account",
+            // Include default values for form inputs
+            "firstname" => "",
+            "lastname" => "",
+            "email" => "",
+            "dateofbirth" => "",
+            "password1" => "",
+            "password2" => ""
+        ];
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            // The user submitted the registration form
+
+            // Get and validate first name
+            $data["firstname"] = $_POST["firstname"];
+            if($data["firstname"] == "") {
+                $data["firstname_error"] = "First name is required";
+            }
+
+            // Get and validate last name
+            $data["lastname"] = $_POST["lastname"];
+            if($data["lastname"] == "") {
+                $data["lastname_error"] = "Last name is required";
+            }
+
+            // Get and validate password
+            $data["email"] = $_POST["email"];
+            if(!filter_var($data["email"], FILTER_VALIDATE_EMAIL))  {
+                $data["email_error"] = "Invalid email address format";
+            } else {
+                // if email is valid, check if it already exists in the database
+                if ($this->userModel->findUserByEmail($data["email"])) {
+                    $data["email_error"] = "That email address is already taken. Please login.";
+                }
+            }
+
+            // Get and validate date of birth
+            $data["dateofbirth"] = $_POST["dateofbirth"];
+            if(!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $data["dateofbirth"])) {
+                $data["dateofbirth_error"] = "Date of birth must be in [YYYY-MM-DD] format";
+            }
+
+            // Get and validate password
+            $data["password1"] = $_POST["password1"];
+            if(!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/", $data["password1"])) {
+                $data["password1_error"] = "Your password must include a minimum eight characters, at least one uppercase letter, one lowercase letter and one number";
+            }
+
+            $data["password2"] = $_POST["password2"];
+            if($data["password2"] == "") {
+                $data["password2_error"] = "Password confirmation required";
+            }
+            if($data["password2"] != $data["password1"]) {
+                $data["password2_error"] = "Passwords must match";
+            }
+
+            if (empty($data["firstname_error"]) && empty($data["lastname_error"])
+                                                && empty($data["email_error"]) && empty($data["password1_error"])) {
+                if ($this->userModel->register($data)) {
+                    // will run if the user is added to the database
+                    header("location: " . URLROOT . "/users/login");
+                } else {
+                    // will run if the user is not added to the database
+                }
+            }
+
+        }
+        $this->view("user/register", $data);
+    }
+    public function login() {
+        $data = [
+            "title" => "Login",
+        ];
+        $this->view("user/login", $data);
+    }
+    public function update() {
+
+    }
+    public function delete() {
+
+    }
+}
---
+++ web/app/views/user/register.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @var ArrayData $data
+ */
+?>
+<?php require_once(APPROOT . "/views/inc/header.php") ?>
+    <div class="p-5 mb-4 bg-light border rounded-3 text-center container">
+        <h1 class="display-4"><?php echo $data["title"] ?></h1>
+    </div>
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <div class="card card-body bg-light">
+                <form action="<?php echo URLROOT; ?>/user/register" method="POST">
+                    <div class="form-group mb-2">
+                        <label for="fullName">First Name</label>
+                        <input class="form-control <?php echo !empty($data["firstname_error"]) ? "is-invalid" : "" ?>" type="text" id="firstname" name="firstname" value="<?php echo $data["firstname"] ?>">
+                        <?php if(!empty($data["firstname_error"])): ?>
+                            <span class="invalid-feedback"><?php echo $data["firstname_error"] ?></span>
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="form-group mb-2">
+                        <label for="fullName">Last Name</label>
+                        <input class="form-control <?php echo !empty($data["lastname_error"]) ? "is-invalid" : "" ?>" type="text" id="lastname" name="lastname" value="<?php echo $data["lastname"] ?>">
+                        <?php if(!empty($data["lastname_error"])): ?>
+                            <span class="invalid-feedback"><?php echo $data["lastname_error"] ?></span>
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="form-group mb-2">
+                        <label for="email">Email Address</label>
+                        <input class="form-control <?php echo !empty($data["email_error"]) ? "is-invalid" : "" ?>" type="text" id="email" name="email" value="<?php echo $data["email"] ?>">
+                        <?php if(!empty($data["email_error"])): ?>
+                            <span class="invalid-feedback"><?php echo $data["email_error"] ?></span>
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="form-group mb-2">
+                        <label for="fullName">Date of Birth</label>
+                        <input class="form-control <?php echo !empty($data["dateofbirth_error"]) ? "is-invalid" : "" ?>" type="text" id="dateofbirth" name="dateofbirth" value="<?php echo $data["dateofbirth"] ?>">
+                        <?php if(!empty($data["dateofbirth_error"])): ?>
+                            <span class="invalid-feedback"><?php echo $data["dateofbirth_error"] ?></span>
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="form-group mb-2">
+                        <label for="fullName">Password</label>
+                        <input class="form-control <?php echo !empty($data["password1_error"]) ? "is-invalid" : "" ?>" type="password" id="password1" name="password1" value="<?php echo $data["password1"] ?>">
+                        <?php if(!empty($data["password1_error"])): ?>
+                            <span class="invalid-feedback"><?php echo $data["password1_error"] ?></span>
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="form-group mb-2">
+                        <label for="fullName">Confirm Password</label>
+                        <input class="form-control <?php echo !empty($data["password2_error"]) ? "is-invalid" : "" ?>" type="password" id="password2" name="password2" value="<?php echo $data["password2"] ?>">
+                        <?php if(!empty($data["password2_error"])): ?>
+                            <span class="invalid-feedback"><?php echo $data["password2_error"] ?></span>
+                        <?php endif; ?>
+                    </div>
+
+                    <input type="submit" class="btn btn-primary" value="Register">
+                    <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-outline-primary">Have an account? Login</a>
+
+                </form>
+            </div>
+        </div>
+    </div>
+<?php require_once(APPROOT . "/views/inc/footer.php") ?>
