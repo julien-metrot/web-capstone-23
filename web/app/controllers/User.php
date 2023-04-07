ae0e4b4098577c48a63885653a3c7ea87e74cf16
Julien Metrot <jul.metrot@gmail.com>
2023-04-07 10:39:11 -0500

    Julien March31 Added the register and login page

--- web/app/controllers/User.php
+++ web/app/controllers/User.php
@@ -1,18 +1,19 @@
 <?php
-
 class User extends Controller {
     public function __construct() {
         $this->userModel = $this->model("Users");
     }
 
     public function register() {
+        if (isLoggedIn()) {
+            redirect("/pages/home");
+        }
         $data = [
             "title" => "Register an Account",
             // Include default values for form inputs
             "firstname" => "",
             "lastname" => "",
             "email" => "",
-            "dateofbirth" => "",
             "password1" => "",
             "password2" => ""
         ];
@@ -42,12 +43,6 @@ class User extends Controller {
                 }
             }
 
-            // Get and validate date of birth
-            $data["dateofbirth"] = $_POST["dateofbirth"];
-            if(!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $data["dateofbirth"])) {
-                $data["dateofbirth_error"] = "Date of birth must be in [YYYY-MM-DD] format";
-            }
-
             // Get and validate password
             $data["password1"] = $_POST["password1"];
             if(!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/", $data["password1"])) {
@@ -63,10 +58,11 @@ class User extends Controller {
             }
 
             if (empty($data["firstname_error"]) && empty($data["lastname_error"])
-                                                && empty($data["email_error"]) && empty($data["password1_error"])) {
+                                                && empty($data["email_error"]) && empty($data["password1_error"]) && empty($data["password2_error"])) {
                 if ($this->userModel->register($data)) {
                     // will run if the user is added to the database
-                    header("location: " . URLROOT . "/users/login");
+                    flash("register_success", "Welcome! Please login to continue.");
+                    header("location: " . URLROOT . "/user/login");
                 } else {
                     // will run if the user is not added to the database
                 }
@@ -75,16 +71,44 @@ class User extends Controller {
         }
         $this->view("user/register", $data);
     }
+
     public function login() {
+        if (isLoggedIn()) {
+            redirect("/pages/home");
+        }
         $data = [
             "title" => "Login",
+            // Include default values for form inputs
+            "email" => "",
+            "password1" => ""
         ];
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $data["email"] = $_POST["email"];
+            $data["password1"] = $_POST["password1"];
+            $userFromDatabase = $this->userModel->findUserByEmail($data["email"]);
+            if(!empty($userFromDatabase) && password_verify($data["password1"], $userFromDatabase->passwordhash)) {
+                // The email and password matched
+                $_SESSION["user_id"] = $userFromDatabase->user_id;
+                $_SESSION["user_firstname"] = $userFromDatabase->firstname;
+                $_SESSION["user_lastname"] = $userFromDatabase->lastname;
+                $_SESSION["user_email"] = $userFromDatabase->email;
+                flash("login-success", "Good " . getTimeOfDayMessage() . ", " . $_SESSION["user_firstname"] . $_SESSION["user_lastname"] . "!");
+                redirect("/page/home");
+            } else {
+                // One or both were incorrect
+                flash("login-fail", "Email or password you entered is not correct", "alert alert-warning");
+            }
+        }
         $this->view("user/login", $data);
     }
-    public function update() {
 
-    }
-    public function delete() {
-
+    public function logout() {
+        //        session_destroy();
+        unset($_SESSION["user_id"]);
+        unset($_SESSION["user_firstname"]);
+        unset($_SESSION["user_lastname"]);
+        unset($_SESSION["user_email"]);
+        flash("logout-success", "You have been logged out. Have a nice day!");
+        redirect("/user/login");
     }
 }
---
+++ web/app/helpers/session_helpers.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+
+function getTimeOfDayMessage() {
+    date_default_timezone_set('America/Chicago');
+    $date = date("H");
+    if ($date >= 5 && $date < 12) {
+        return "morning";
+    }
+    if ($date >= 12 && $date < 17) {
+        return "afternoon";
+    }
+    if ($date >= 17 && $date < 22) {
+        return "evening";
+    }
+    if ($date >= 22) {
+        return "night";
+    }
+    if ($date < 5) {
+        return "night";
+    }
+}
+
+function flash($firstname, $message = "", $class = "alert alert-success") {
+    if (!empty($firstname)) {
+        if (!empty($message) && empty($_SESSION[$firstname])) {
+            if (!empty($_SESSION[$firstname])) {
+                unset($_SESSION[$firstname]);
+            }
+            $_SESSION[$firstname] = $message;
+            if (!empty($_SESSION[$firstname . "_class"])) {
+                unset($_SESSION[$firstname . "_class"]);
+            }
+            $_SESSION[$firstname . "_class"] = $class;
+        } else if(empty($message) && !empty($_SESSION[$firstname])) {
+            if (!empty($_SESSION[$firstname . "_class"])) {
+                $class = $_SESSION[$firstname . "_class"];
+            } else {
+                $class = "";
+            }
+            echo '<div class="container"><div class="' . $class . '" id="msg-flash">' . $_SESSION[$firstname] . '</div></div>';
+            unset($_SESSION[$firstname]);
+            unset($_SESSION[$firstname . "_class"]);
+        }
+    }
+}
+
+function isLoggedIn() {
+    return isset($_SESSION["user_id"]) && isset($_SESSION["user_firstname"]) && isset($_SESSION["user_lastname"]) && isset($_SESSION["user_email"]);
+}
