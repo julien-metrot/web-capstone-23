3a331da1a869a334125c3f8ef556fd4659b02d56
April Rieff <april.rieff@gmail.com>
2023-05-05 11:45:55 -0500

    approve and deny functionality

--- web/app/models/Adoptmodel.php
+++ web/app/models/Adoptmodel.php
@@ -8,12 +8,6 @@ class Adoptmodel
         $this->db = new Database();
     }
 
-    public function getAnimal($id){
-        $this->db->query("SELECT * FROM animal WHERE animal_id = :animal_id");
-        $this->db->bind(":animal_id", $id);
-        return $this->db->single();
-    }
-
     public function getAllApplications() {
         $this->db->query("
             SELECT u.firstname, u.lastname, a.animal_id, a.name, app.application_date, u.user_id, app.application_status, app.application_id
@@ -77,25 +71,21 @@ class Adoptmodel
         return $this->db->single();
     }
 
-    public function apply($apply) {
-//        echo "<pre>" . print_r($apply, 1 ) . "</pre>";
-//        die();
-        $this->db->query("INSERT INTO application(employer_name, has_children, home_status, landlord_name, landlord_phone, current_pets, animal_id, user_id) VALUES(:employer_name, :has_children, :home_status, :landlord_name, :landlord_phone, :current_pets, :animal_id, :user_id)");
-        $this->db->bind(":employer_name", $apply["employerName"]);
-        $this->db->bind(":has_children", $apply["hasChildren"]);
-        $this->db->bind(":home_status", $apply["homeStatus"]);
-        $this->db->bind(":landlord_name", $apply["landlordName"]);
-        $this->db->bind(":landlord_phone", $apply["landlordName"]);
-        $this->db->bind(":current_pets", $apply["currentPets"]);
-        $this->db->bind(":animal_id", $apply['animal_id']);
-        $this->db->bind(":user_id", $_SESSION['user_id']);
+    public function apply($data) {
+        $this->db->query("INSERT INTO application(employer_name, has_children, home_status, landlord_name, landlord_phone, current_pets) VALUES(:employer_name, :has_children, :home_status, :landlord_name, :landlord_phone, :current_pets)");
+        $this->db->bind(":employer_name", $data["employerName"]);
+        $this->db->bind(":has_children", $data["hasChildren"]);
+        $this->db->bind(":home_status", $data["homeStatus"]);
+        $this->db->bind(":landlord_name", $data["landlordName"]);
+        $this->db->bind(":landlord_phone", $data["landlordName"]);
+        $this->db->bind(":current_pets", $data["currentPets"]);
         return $this->db->execute(); // will return true or false if the database processed the query
     }
 
-    public function approve() {
-        $this->db->query("UPDATE application
-        SET application_status = 'Approved'
-        WHERE application.user_id = user.user_id");
+    public function approveApplication($application_id) {
+        $this->db->query("UPDATE application SET application_status = 'Approved' WHERE application_id = :application_id");
+        $this->db->bind(":application_id", $application_id );
+        return $this->db->execute();
     }
 
     public function denyApplication($application_id) {
@@ -103,29 +93,4 @@ class Adoptmodel
         $this->db->bind(":application_id", $application_id );
         return $this->db->execute();
     }
-
-    public function editApplication($data)
-    {
-//        echo "<pre>" . print_r($data, 1 ) . "</pre>";
-//        die();
-        $this->db->query("UPDATE application SET current_pets = :current_pets, has_children= :has_children, employer_name = :employer_name, home_status = :home_status, landlord_name = :landlord_name, landlord_phone = :landlord_phone WHERE application_id = :application_id");
-        $this->db->bind(":application_id", $data["application_id"]);
-        $this->db->bind(":current_pets", $data["currentPets"]);
-        $this->db->bind(":has_children", $data["hasChildren"]);
-        $this->db->bind(":employer_name", $data["employerName"]);
-        $this->db->bind(":home_status", $data["homeStatus"]);
-        $this->db->bind(":landlord_name", $data["landlordName"]);
-        $this->db->bind(":landlord_phone", $data["landlordPhone"]);
-        return $this->db->execute();
-    }
-
-    public function deleteApplication($application_id)
-    {
-//        echo "<pre>" . print_r($application_id, 1 ) . "</pre>";
-//        die();
-        $this->db->query("DELETE FROM application WHERE application_id = :application_id");
-        $this->db->bind(":application_id", $application_id);
-        return $this->db->execute();
-    }
-
 }
